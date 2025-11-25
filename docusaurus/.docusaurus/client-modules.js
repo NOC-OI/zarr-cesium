ee6f63f3cd1be6c69fc4f25aaec3d50bc98623d2
tobias ferreira <soutobias@yahoo.com.br>
2025-11-25 02:41:45 +0000

    Update docusaurus

--- docusaurus/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/mnt/noc/code/atlantis/frontend/zarr-cesium/docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/mnt/noc/code/atlantis/frontend/zarr-cesium/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/mnt/noc/code/atlantis/frontend/zarr-cesium/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/mnt/noc/code/atlantis/frontend/zarr-cesium/docusaurus/assets/custom.css"),
-];
--- docusaurus/build/assets/js/aba21aa0.ffaceba7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkzarr_cesium_docs=self.webpackChunkzarr_cesium_docs||[]).push([[5742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
